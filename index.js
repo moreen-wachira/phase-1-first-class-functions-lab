f957d224aab98b65114339b5dbb0630a8c0ba96b
Moreen <moreencharles@gmail.com>
2023-10-06 02:24:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const drivers = ['Antonia', 'Amaria', 'Nuru', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = function (fareMultiplier) {
+  return function(value) {
+    return fareMultiplier * value;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function fetchSpecifiedDrivers(drivers, func) {
+  return func(drivers);
+}
+
+const selectDifferentDrivers = function(drivers) {
+    return drivers.slice(0,2);
+  }
+const selectDifferentDrivers2 = function(drivers) {
+    return drivers.slice(-2);
+ }
+
 // Code your solution in this file!
